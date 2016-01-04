e5a9f583db687a964f48e208afa8dbecb71227cf
Patrick Sullivan <p.sullivan@zoho.com>
2016-01-04 01:04:41 -0500

    Added empty test classes. Modified build path.

---
+++ test/designParser/visitor/ClassFieldVisitorTest.java
@@ -0,0 +1,14 @@
+package designParser.visitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClassFieldVisitorTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ test/designParser/visitor/ClassMethodVisitorTest.java
@@ -0,0 +1,14 @@
+package designParser.visitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClassMethodVisitorTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
