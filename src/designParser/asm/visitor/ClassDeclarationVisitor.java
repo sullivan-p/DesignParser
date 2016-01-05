f66ea0c473aa607ee46bbc4c1a3e0d770208ef04
Navin Weiss <weissna@rose-hulman.edu>
2016-01-05 02:23:35 -0500

    Overwriting ClassDeclarationVisitor and Model

--- src/designParser/asm/visitor/ClassDeclarationVisitor.java
+++ src/designParser/asm/visitor/ClassDeclarationVisitor.java
@@ -19,10 +19,7 @@ public class ClassDeclarationVisitor extends ModelBuilderClassVisitor {
 
 	@Override
 	public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-		// TODO: delete the line below
 		System.out.println("Class: " + name + " extends " + superName + " implements " + Arrays.toString(interfaces));
-		// TODO: construct an internal representation of the class for later use
-		// by decorators
 		super.visit(version, access, name, signature, superName, interfaces);
 	}
 
--- src/designParser/model/impl/Model.java
+++ src/designParser/model/impl/Model.java
@@ -1,16 +1,21 @@
 package designParser.model.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import designParser.model.api.IModel;
 import designParser.model.api.IObjOrientedEntity;
 import designParser.model.visitor.IModelVisitor;
 
-public class Model implements IModel{
-    private List<IObjOrientedEntity> entities;
+public class Model implements IModel {
+	private List<IObjOrientedEntity> entities;
 
-    @Override
-    public void accept(IModelVisitor visitor) {
-        // TODO Auto-generated method stub
-    }
+	public Model() {
+		entities = new ArrayList<IObjOrientedEntity>();
+	}
+
+	@Override
+	public void accept(IModelVisitor visitor) {
+		// TODO Auto-generated method stub
+	}
 }
