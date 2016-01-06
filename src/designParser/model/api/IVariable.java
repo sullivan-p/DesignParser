2af38f9cf531b01474a31c927b52d8d3565deb0f
Patrick Sullivan <p.sullivan@zoho.com>
2016-01-06 00:29:25 -0500

    Variable model can now contain multiple data types.

--- src/designParser/model/api/IVariable.java
+++ src/designParser/model/api/IVariable.java
@@ -1,5 +1,14 @@
 package designParser.model.api;
 
+import java.util.Collection;
+
 public interface IVariable extends IModelComponent {
-    public IDataType getType();
+    
+    /**
+     * If the variable is not a generic data type, return a single-element
+     * container that contains only the variable's data type. If the variable is 
+     * a generic type, return a collection containing the variable's type in 
+     * addition to any type parameters.
+     */
+    public Collection<IDataType> getTypes();
 }
--- src/designParser/model/impl/VariableModel.java
+++ src/designParser/model/impl/VariableModel.java
@@ -1,16 +1,25 @@
 package designParser.model.impl;
 
+import java.util.Collection;
+
 import designParser.model.api.IDataType;
 import designParser.model.api.IVariable;
 import designParser.model.visitor.IModelVisitor;
 
 public class VariableModel implements IVariable {
     private String name;
-    private IDataType type;
+    private Collection<IDataType> types;
 
-    public VariableModel(String name, IDataType type) {
+    /**
+     * @param name
+     * @param types If the variable is not a generic data type, should be a 
+     * single-element collection that contains only the variable's data type. 
+     * If the variable is a generic type, should be a list that contains the 
+     * variable's type in addition to any type parameters.
+     */
+    public VariableModel(String name, Collection<IDataType> types) {
         this.name = name;
-        this.type = type;
+        this.types = types;
     }
 
     @Override
@@ -19,8 +28,8 @@ public class VariableModel implements IVariable {
     }
 
     @Override
-    public IDataType getType() {
-        return type;
+    public Collection<IDataType> getTypes() {
+        return types;
     }
     
     @Override
