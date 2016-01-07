bacb0a13ad2bde61dbd9ff365713611ad355839e
Navin Weiss <weissna@rose-hulman.edu>
2016-01-07 08:49:04 -0500

    Added some arrow test cases. Extends still needs to be done

---
+++ test/designParser/visitor/ExtendsArrowTest.java
@@ -0,0 +1,22 @@
+package designParser.visitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExtendsArrowTest {
+	
+	private final static String[] OBJECT_NAMES = { "appLauncherSltn.AppLauncherApplication",
+			"appLauncherSltn.ApplicationLauncher", "appLauncherSltn.DataFileRunner",
+			"appLauncherSltn.DirectoryChangeLogger", "appLauncherSltn.DirectoryEvent",
+			"appLauncherSltn.DirectoryMonitorService", "appLauncherSltn.ExecutableFileRunner",
+			"appLauncherSltn.IApplicationLauncher", "appLauncherSltn.IDirectoryListener",
+			"appLauncherSltn.IDirectoryMonitorService", "appLauncherSltn.ProcessRunner"
+	};
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
