d8985fee51daea387c21f3fa73d29aa4be53949a
Patrick Sullivan <p.sullivan@zoho.com>
2016-01-13 02:47:55 -0500

    Added Pair data type.

---
+++ src/pair/impl/Pair.java
@@ -0,0 +1,65 @@
+package pair.impl;
+
+import pair.api.IPair;
+
+public class Pair<T, U> implements IPair<T, U> {
+    private T first;
+    private U second;
+    
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public T getFirst() {
+        return first;
+    }
+
+    @Override
+    public U getSecond() {
+        return second;
+    }
+
+    @Override
+    public void setFirst(T first) {
+        this.first = first;        
+    }
+
+    @Override
+    public void setSecond(U second) {
+        this.second = second;        
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pair other = (Pair) obj;
+        if (first == null) {
+            if (other.first != null)
+                return false;
+        } else if (!first.equals(other.first))
+            return false;
+        if (second == null) {
+            if (other.second != null)
+                return false;
+        } else if (!second.equals(other.second))
+            return false;
+        return true;
+    }
+}
